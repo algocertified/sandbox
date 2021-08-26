4eae340f50a1db2157a9fc15e589ada98af33c42
Hardik Vala <hardikvala24@gmail.com>
2021-08-25 17:36:26 -0700

    Create happy path test case for initial sale

---
+++ tiquet/py/tests/test_initial_sale.py
@@ -0,0 +1,57 @@
+import base64
+import pytest
+
+from algosdk.error import AlgodHTTPError
+from algosdk.future import transaction
+from fixtures import *
+from tiquet.common.algorand_helper import AlgorandHelper
+from tiquet.tiquet_client import TiquetClient
+from tiquet.tiquet_issuer import TiquetIssuer
+
+
+def test_initial_sale_success(
+    accounts, algodclient, algod_params, algorand_helper, logger
+):
+    # Get issuer algorand account, with public and secret keys.
+    issuer_account = accounts.get_issuer_account()
+    logger.debug("Issuer address: {}".format(issuer_account["pk"]))
+
+    issuer = TiquetIssuer(
+        pk=issuer_account["pk"],
+        sk=issuer_account["sk"],
+        mnemonic=issuer_account["mnemonic"],
+        # TODO(hv): Move TEAL source paths to config
+        app_fpath="/root/tiquet/teal/tiquet_app.teal",
+        clear_fpath="/root/tiquet/teal/clear.teal",
+        escrow_fpath="/root/tiquet/teal/tiquet_law.teal",
+        algodclient=algodclient,
+        algod_params=algod_params,
+        logger=logger,
+    )
+
+    tiquet_id, app_id, escrow_lsig = issuer.issue_tiquet()
+
+    logger.debug("Tiquet Id: {}".format(tiquet_id))
+    logger.debug("App Id: {}".format(app_id))
+    logger.debug("Escrow address: {}".format(escrow_lsig.address()))
+
+    buyer_account = accounts.get_buyer_account()
+    logger.debug("Buyer address: {}".format(buyer_account["pk"]))
+
+    buyer = TiquetClient(
+        pk=buyer_account["pk"],
+        sk=buyer_account["sk"],
+        mnemonic=buyer_account["mnemonic"],
+        algodclient=algodclient,
+        algod_params=algod_params,
+        logger=logger,
+        escrow_lsig=escrow_lsig
+    )
+
+    buyer.buy_tiquet(
+        tiquet_id=tiquet_id,
+        app_id=app_id,
+        issuer_account=issuer_account["pk"],
+        seller_account=issuer_account["pk"],
+        amount=1000000000000000,
+    )
